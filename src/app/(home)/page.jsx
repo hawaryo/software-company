324edbc53047df547c3c9becdc83a99e645c7a5a
hawaryo <skarmar50@gmail.com>
2024-12-16 03:46:54 +0200

    feat: adding Home page feature  section

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.scss";
 import HomeHeader from "@/components/home/HomeHeader";
+import FeaturesSection from "@/components/home/FeaturesSection";
 export const metadata = {
   title: "Software Company",
   description: "Welcome to software company",
@@ -9,6 +10,9 @@ export default function HomePage() {
   return (
     <>
       <HomeHeader />
+      <main>
+        <FeaturesSection />
+      </main>
     </>
   );
 }
